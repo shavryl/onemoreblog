bbfba86a97e7a34b9599efeeb891e0552d715d8d
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-11-17 18:30:12 +0200

    html things wo css and pagination

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 
 
 def post_list(request):
-    posts = Post.published.all()
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html',
-                  {'posts': posts})
+                  {'page': page, 'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
@@ -15,3 +24,5 @@ def post_detail(request, year, month, day, post):
                              publish__day=day)
     return render(request, 'blog/post/detail.html',
                   {'post': post})
+
+
