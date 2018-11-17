5b5fb66d35cd5a98211bd02498139c193fdbeccf
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-11-17 16:03:14 +0200

    model manager and simple views

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,12 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset() \
+            .filter(status='published')
+
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -24,3 +30,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    objects = models.Manager()
+    published = PublishedManager()
