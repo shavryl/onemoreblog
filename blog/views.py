0f231c8fb2ab8df077ccd9585edf1492cb02740e
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-11-18 09:24:30 +0200

    view and url for sharing post by email

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.views.generic import ListView
+from django.core.mail import send_mail
 from .models import Post
 from .forms import EmailPostForm
 
@@ -22,13 +23,23 @@ def post_detail(request, year, month, day, post):
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
 
     if request.method == 'POST':
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            # ... send email
+            post_url = request.build_absolute_uri(
+                        post.get_absolute_url())
+            subject = '{} ({} recommends you reading "{}"'\
+                    .format(cd['name'], cd['email'], post.title)
+            message = 'Read "{}" at {}\n\n{}\'s comments: {}'\
+                    .format(post.title, post_url, cd['name'], cd['comments'])
+            send_mail(subject, message,
+                      'gavrilchenko.stanislav@gmail.com', [cd['to']])
+            sent = True
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html',
-                  {'post': post, 'form': form})
+                  {'post': post, 'form': form,
+                   'sent': sent})
