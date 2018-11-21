0e4e617df080887d8122451ebb577715431ac9a8
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-11-21 20:20:20 +0200

    postgreSQL and requirements

--- onemoreblog/settings.py
+++ onemoreblog/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'onemoreblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'blog',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
     }
 }
 
